e6f619a86b294dad5deffdb54536f824feb0e919
Pablo Piaggio <pablo.piaggio@alchemer.com>
2021-12-07 21:31:46 -0600

    Add all things day 7

---
+++ day07/part2.py
@@ -0,0 +1,25 @@
+def fuel_comsuption(pos1, pos2):
+    n = abs(pos1 - pos2)
+    return n * (n + 1) // 2
+
+def solution(filename):
+    with open(filename) as fp:
+        raw_data = fp.read()
+
+    crab_positions = list(map(int, raw_data.split(',')))
+
+    fuel_per_position = []
+    for candidate_position in range(min(crab_positions), max(crab_positions)):
+        current_fuel_comsuption = 0
+        for crab_position in crab_positions:
+            current_fuel_comsuption += fuel_comsuption(crab_position, candidate_position)
+        fuel_per_position.append(current_fuel_comsuption)
+
+    return min(fuel_per_position)
+
+if __name__ == "__main__":
+    result = solution("./example.txt")
+    print(result)   # it should be 37
+
+    result = solution("./input.txt")
+    print(result)
