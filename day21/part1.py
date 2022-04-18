83ffbd27b29e53c5d967e3da7cb87b40f3ca9000
Pablo Piaggio <pablo.piaggio@alchemer.com>
2022-04-18 16:59:56 -0500

    Adhere to actual die rules

--- day21/part1.py
+++ day21/part1.py
@@ -1,29 +1,41 @@
+WIN_POINTS = 1000
+
+
+def die_rolls():
+    value = 1
+    while True:
+        yield value
+        value = (value) % 100 + 1
+
+
 def solution(position1, position2):
+    game_die_iterator = die_rolls()
+
+    def die():
+        return next(game_die_iterator)
+
     score1 = score2 = 0
-    play = 0
     rolled_dice = 0
 
     while True:
-        dice1 = 3 * (6 * play + 2)
+        dice1 = die() + die() + die()
         position1 = (position1 - 1 + dice1) % 10 + 1
-        rolled_dice += 3
         score1 += position1
-        if score1 >= 1000:
+        rolled_dice += 3
+        if score1 >= WIN_POINTS:
             return rolled_dice * score2
 
-        dice2 = 3 * (6 * play + 5)
+        dice2 = die() + die() + die()
         position2 = (position2 - 1 + dice2) % 10 + 1
         score2 += position2
         rolled_dice += 3
-        if score2 >= 1000:
+        if score2 >= WIN_POINTS:
             return rolled_dice * score1
-
-        play += 1
 
 
 if __name__ == "__main__":
     result = solution(4, 8)
-    print(result)
+    print(result)  # it should be 739785
 
     result = solution(1, 5)
     print(result)
